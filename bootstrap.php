cb3d05b375f8f368cd119157004dd5a7c210485c
bhupinder <bhindekang199698@gmail.com>
2020-08-19 19:46:16 -0400

    password

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 require'database/connection.php';
 require'database/queryBuilder.php';
-
+$config= require'config.php';
 return new queryBuilder(
 
-Connection::make()
+Connection::make($config['database'])
 
 );
 ?>
